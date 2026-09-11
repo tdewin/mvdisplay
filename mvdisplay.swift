c95e229f843b0c892e756eb3a3c71f68f872271c
tdewin <tdewin@users.noreply.github.com>
2026-09-11 10:32:12 +0200

    add mirror support

--- mvdisplay.swift
+++ mvdisplay.swift
@@ -1,9 +1,9 @@
 import CoreGraphics
 
-// Usage: mvdisplay <left|right|top|bottom> [offset]
+// Usage: mvdisplay <left|right|top|bottom|mirror> [offset]
 let args = CommandLine.arguments
 guard args.count > 1 else {
-    print("Usage: mvdisplay <left|right|top|bottom> [offset]")
+    print("Usage: mvdisplay <left|right|top|bottom|mirror> [offset]")
     exit(1)
 }
 
@@ -25,35 +25,45 @@ guard let externalDisplay = displayIDs.first(where: { $0 != mainDisplay }) else 
     exit(1)
 }
 
-let mainWidth = Int32(CGDisplayPixelsWide(mainDisplay))
-let mainHeight = Int32(CGDisplayPixelsHigh(mainDisplay))
-let extWidth = Int32(CGDisplayPixelsWide(externalDisplay))
-let extHeight = Int32(CGDisplayPixelsHigh(externalDisplay))
-
-var targetX: Int32 = 0
-var targetY: Int32 = 0
-
-switch direction {
-case "left":
-    targetX = -extWidth
-    targetY = offset // Negative moves UP, Positive moves DOWN
-case "right":
-    targetX = mainWidth
-    targetY = offset // Negative moves UP, Positive moves DOWN
-case "top":
-    targetX = offset // Negative moves LEFT, Positive moves RIGHT
-    targetY = -extHeight
-case "bottom":
-    targetX = offset // Negative moves LEFT, Positive moves RIGHT
-    targetY = mainHeight
-default:
-    print("Invalid direction. Use: left, right, top, or bottom.")
-    exit(1)
-}
-
 var config: CGDisplayConfigRef?
 CGBeginDisplayConfiguration(&config)
-CGConfigureDisplayOrigin(config, externalDisplay, targetX, targetY)
-CGCompleteDisplayConfiguration(config, .permanently)
 
-print("Secondary display set to \(direction) with offset \(offset).")
+if direction == "mirror" {
+    // Mirror external display to main display
+    CGConfigureDisplayMirrorOfDisplay(config, externalDisplay, mainDisplay)
+    print("Secondary display set to mirror primary display.")
+} else {
+    // Un-mirror secondary display if currently mirrored
+    CGConfigureDisplayMirrorOfDisplay(config, externalDisplay, kCGNullDirectDisplay)
+
+    let mainWidth = Int32(CGDisplayPixelsWide(mainDisplay))
+    let mainHeight = Int32(CGDisplayPixelsHigh(mainDisplay))
+    let extWidth = Int32(CGDisplayPixelsWide(externalDisplay))
+    let extHeight = Int32(CGDisplayPixelsHigh(externalDisplay))
+
+    var targetX: Int32 = 0
+    var targetY: Int32 = 0
+
+    switch direction {
+    case "left":
+        targetX = -extWidth
+        targetY = offset // Negative moves UP, Positive moves DOWN
+    case "right":
+        targetX = mainWidth
+        targetY = offset // Negative moves UP, Positive moves DOWN
+    case "top":
+        targetX = offset // Negative moves LEFT, Positive moves RIGHT
+        targetY = -extHeight
+    case "bottom":
+        targetX = offset // Negative moves LEFT, Positive moves RIGHT
+        targetY = mainHeight
+    default:
+        print("Invalid direction. Use: left, right, top, bottom, or mirror.")
+        exit(1)
+    }
+
+    CGConfigureDisplayOrigin(config, externalDisplay, targetX, targetY)
+    print("Secondary display set to \(direction) with offset \(offset).")
+}
+
+CGCompleteDisplayConfiguration(config, .permanently)
